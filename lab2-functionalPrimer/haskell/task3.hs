cd5326b9eb7b4db45a057b0058af330cdb796766
sarasbtb <saratalebsbai@gmail.com>
2026-02-03 12:24:08 +0000

    task 4 completed and 5 started

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -8,8 +8,13 @@ sgnGuards x | x < 0 = -1
             | x == 0 = 0
             | otherwise = 1
 
+grade score | score >= 90 = "A"
+            | score >= 80 = "B"
+            | score >= 70 = "C"
+            | otherwise = "F"
+
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (sgnGuards (x)) )
